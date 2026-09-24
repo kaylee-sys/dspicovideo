0bfcef99aea35854e488571b9fc5841cfc669eb5
kaylee-sys <jannahapetyan32@gmail.com>
2026-09-24 14:09:09 +0500

    Delete arm7 directory

--- arm7/source/Arm7State.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-/// @brief Enum representing the arm7 state.
-enum class Arm7State
-{
-    Idle,
-    ExitRequested
-};
--- arm7/source/common.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <libtwl/rtos/rtosMutex.h>
-
-extern rtos_mutex_t gCardMutex;
